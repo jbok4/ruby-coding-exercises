ebc0ee7033cee61fc89904025c5571825590831c
jbok4 <jbok4@aol.com>
2017-03-30 10:03:59 -0400

    converting 2 arrays into a hash

--- january/17.rb
+++ january/17.rb
@@ -1,6 +1,12 @@
 require 'rspec'
+# https://www.crondose.com/2017/01/generating-hash-two-arrays-ruby/
 
 def title_builder headers, data
+  hash = {}
+  headers.each_with_index do |header, i|
+    hash[header] = data[i]
+  end  
+  hash
 end
 
 describe 'Hash builder' do
