6b345e150a21072af5aaa585edca054e9b5fdc04
jbok4 <jbok4@aol.com>
2017-03-30 08:45:49 -0400

    average value of an array

--- january/14.rb
+++ january/14.rb
@@ -1,4 +1,11 @@
 require 'rspec'
+# https://www.crondose.com/2017/01/finding-average-value-array-ruby/
+
+class Array
+  def average
+    inject(&:+) / length
+  end
+end
 
 describe 'Calculating Average' do
   it 'returns the average value from an array' do
