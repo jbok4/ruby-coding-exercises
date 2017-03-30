b9ffcd206572bf4f51b038d2018026fd34c5644c
jbok4 <jbok4@aol.com>
2017-03-30 09:01:47 -0400

    reverse string without using reverse method

--- january/15.rb
+++ january/15.rb
@@ -1,4 +1,16 @@
 require 'rspec'
+# https://www.crondose.com/2017/01/reverse-letters-string-without-using-reverse-method-ruby/
+
+class String
+  def alt_reverse
+    new_string = ''
+    string_length = self.length -1
+    string_length.downto(0).each do |i|
+      new_string << self[i]
+    end
+    new_string
+  end
+end
 
 describe 'Letter reversing' do
   it 'reverses the letters of a string without using the reverse method' do
